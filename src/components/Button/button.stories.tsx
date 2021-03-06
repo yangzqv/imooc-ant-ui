a06ed5d0c608d7959ad4c4f6d7321d207a154418
yangzqv <yangzqv@126.com>
2021-03-06 19:52:31 +0800

    feature: add Button storybook

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -4,7 +4,22 @@ import { action } from '@storybook/addon-actions';
 
 import Button from './button';
 
-const defaultButton = () => <Button onClick={action('clicked')}>default Button</Button>
+const defaultButton = () => <Button onClick={action('clicked')}>default Button</Button>;
+const buttonWithSize = () => (
+  <>
+    <Button size="lg">large button</Button>
+    <Button size="sm">small button</Button>
+  </>
+);
+const buttonWithType = () => (
+  <>
+    <Button btnType="primary">primary button</Button>
+    <Button btnType="danger">danger button</Button>
+    <Button btnType="link">link button</Button>
+  </>
+)
 
 storiesOf('Button Component', module)
-  .add('默认 Button', defaultButton);
+  .add('默认 Button', defaultButton)
+  .add('不同尺寸的 Button', buttonWithSize)
+  .add('不同类型的 Button', buttonWithType)
