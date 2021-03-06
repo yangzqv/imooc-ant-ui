e4618642c96595c5d8240afcf75efdc55d38f9a6
yangzqv <yangzqv@126.com>
2021-03-06 19:14:33 +0800

    featu

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -4,7 +4,7 @@ import { action } from '@storybook/addon-actions';
 
 import Button from './button';
 
-const defaultButton = () => <Button onClick={action('clicked')}>default Button</Button>;
+const defaultButton = () => <Button onClick={action('clicked')}>default Button</Button>
 
 storiesOf('Button Component', module)
-  .add('默认 Button', defaultButton)
+  .add('默认 Button', defaultButton);
